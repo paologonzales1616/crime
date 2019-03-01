6b7e8243c2a3a1f46b19c3bf374baada2c138e54
Samurai <paologonzales1616@gmail.com>
2019-03-01 20:46:32 +0800

    update build

---
+++ build/precache-manifest.95e664c1792fe133fb557606cc5a7612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ab445c7ff5ead179f44",
+    "url": "/static/css/main.5940dbf3.chunk.css"
+  },
+  {
+    "revision": "7ab445c7ff5ead179f44",
+    "url": "/static/js/main.7ab445c7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "34da195f898913180787",
+    "url": "/static/css/2.dda95699.chunk.css"
+  },
+  {
+    "revision": "34da195f898913180787",
+    "url": "/static/js/2.34da195f.chunk.js"
+  },
+  {
+    "revision": "8418f109063e65ff4bd74f58c772b2b1",
+    "url": "/index.html"
+  }
+];
