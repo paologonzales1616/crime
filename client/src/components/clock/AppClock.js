9923fd604fbf9bb92bfb9187c7bd8321e4c921f7
paolo gonzales <paologonzales1616@gmail.com>
2019-03-04 20:56:16 +0800

    clock update timezone

--- client/src/components/clock/AppClock.js
+++ client/src/components/clock/AppClock.js
@@ -6,8 +6,8 @@ import './AppClock.css'
 const AppClock = () => {
     return (
         <Row className="app-clock-div text-center">
-            <Clock format="HH:mm:ss" ticking={true} interval={1000} className="app-clock" />
-            <Clock format={'MMMM M, YYYY dddd'} className="app-date" />
+            <Clock format="HH:mm:ss" ticking={true} interval={1000} timezone={'Asia/Manila'} className="app-clock" />
+            <Clock format={'MMMM M, YYYY dddd'} timezone={'Asia/Manila'} className="app-date" />
         </Row>
     )
 }
