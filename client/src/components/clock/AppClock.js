8b8404f6a0875ee6f25031ddf850d1a160d7eef7
paolo gonzales <paologonzales1616@gmail.com>
2019-03-06 01:02:57 +0800

    clock

--- client/src/components/clock/AppClock.js
+++ client/src/components/clock/AppClock.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Clock from 'react-live-clock'
 import { Row } from 'reactstrap'
 import './AppClock.css'
+const date = Date.now()
 
 const AppClock = () => {
     return (
         <Row className="app-clock-div text-center">
             <Clock format="HH:mm:ss" ticking={true} interval={1000} timezone={'Asia/Manila'} className="app-clock" />
-            <Clock format={'MMMM M, YYYY dddd'} timezone={'Asia/Manila'} className="app-date" />
+            <Clock format={'MMMM Mo, YYYY dddd'} timezone={'Asia/Manila'} className="app-date" />
         </Row>
     )
 }
