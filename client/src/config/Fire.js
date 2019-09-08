9e528a4aeb1d59ba6ce87ac65eb3e74e3a7ec5c8
paolo gonzales <paologonzales1616@gmail.com>
2019-09-08 12:25:42 +0800

    New firebase key

--- client/src/config/Fire.js
+++ client/src/config/Fire.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase'
 const config = {
-    apiKey: "AIzaSyADMg6OCHwf6ZC8QaMtpCb-nddpnSleJx8",
-    authDomain: "crime-fd040.firebaseapp.com",
-    databaseURL: "https://crime-fd040.firebaseio.com",
-    projectId: "crime-fd040",
-    storageBucket: "crime-fd040.appspot.com",
-    messagingSenderId: "123884288015"
+    apiKey: "AIzaSyB1IbQ5S9pKxFBX2afRpUYXiJlE9jzf-Bw",
+    authDomain: "crime-e40c1.firebaseapp.com",
+    databaseURL: "https://crime-e40c1.firebaseio.com",
+    projectId: "crime-e40c1",
+    storageBucket: "",
+    messagingSenderId: "778098789673",
+    appId: "1:778098789673:web:ac39e117b39efd42dc3b38"
 };
 const fire = firebase.initializeApp(config)
 export default fire
